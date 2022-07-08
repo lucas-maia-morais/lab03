781b84382ec5bb4484142a405631aa5e78cbde61
lucas-maia-morais <lucasmaiamorais@gmail.com>
2022-07-08 20:52:44 -0300

    Final Builder

--- README.md
+++ README.md
@@ -136,6 +136,105 @@ Tradicional e Inesperado, Gateau aux Carrottes!
 ```
 
 ## Builder Padaria
-![Alt text](./padaria-builder.drawio.svg)
+
+### Identificação das classes
+
+No caso da implementação utilizando builder seria trivial construir um único builder e salvar o tipo de bolo e o recheio utilizando o diretor, porém para fazer mais sentido o uso de classes vamos supor que criar diferentes tipos bolo tenha procedimentos muito diferentes e faremos um procedimento no diretor para cada tipo, e a os recheios do bolo ficaram sendo feito pelo builder:
+
+Procedimentos:
+ - bakeAniversario
+ - bakeCasamento
+ - bakeInformal
+
+Builder:
+ - bakeChocolate
+ - bakeMandioca
+ - bakeCenoura
+
+### Diagrama de classes
+O diagrama de classes para o sistema da padaria usando o builde imaginado está descrito abaixo
+![Alt text](./padaria-builder.svg)
 <!-- <img src="./padaria-builder.drawio.svg">
  -->
+
+### Exemplo de caso
+Para uma main descrita abaixo que funciona como cliente:
+
+```
+if __name__ == "__main__":
+
+    director = Director()
+
+## ========== Chocolate ===================================
+    choco = ChocolateBuilder()
+    print("Bolos de Chocolate")
+    print("=============================")
+    director.builder = choco
+
+    director.bakeAniversario()
+    print(str(choco.product))
+
+    director.bakeCasamento()
+    print(choco.product)
+
+    director.bakeInformal()
+    print(choco.product)
+    print("\n")
+
+## ========== Mandioca ===================================
+    mandioca = MandiocaBuilder()
+    print("Bolos de Mandioca")
+    print("=============================")
+    director.builder = mandioca
+
+    director.bakeAniversario()
+    print(mandioca.product)
+
+    director.bakeCasamento()
+    print(mandioca.product)
+
+    director.bakeInformal()
+    print(Builder.product)
+    print("\n")
+
+## ========== Cenoura ===================================
+    cenoura = CenouraBuilder()
+    print("Bolos de Cenoura")
+    print("=============================")
+    director.builder = cenoura
+
+    director.bakeAniversario()
+    print(cenoura.product)
+
+    director.bakeCasamento()
+    print(cenoura.product)
+
+    director.bakeInformal()
+    print(cenoura.product)
+    print("\n")
+```
+
+Temos o output esperado a seguir:
+
+```
+Bolos de Chocolate
+=============================
+Bolo de chocolate do tipo Aniversário com Vela
+Bolo de chocolate do tipo Casamento com Boneco
+Bolo de chocolate do tipo Informal com 
+
+
+Bolos de Mandioca
+=============================
+Bolo de Mandioca do tipo Aniversário com Vela
+Bolo de Mandioca do tipo Casamento com Boneco
+<property object at 0x7f4d8b244cc0>
+
+
+Bolos de Cenoura
+=============================
+Bolo de Cenoura do tipo Aniversário com Vela
+Bolo de Cenoura do tipo Casamento com Boneco
+Bolo de Cenoura do tipo Informal com 
+
+```
--- padaria-builder.py
+++ padaria-builder.py
@@ -1,43 +1,219 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import Any
 
-class Padaria():
-    def __init__(self, builder):
-        self.builder = builder
+# Builder Abstrato
+# -------------------------------------------------------
 
-    def bake(self):
-        return self.builder.bake()
-
-class Bolo():
-    def __init__(self, recheio, tipo):
-        self.recheio = recheio
-        self.tipo = tipo
-
-class builder(ABC):
-    @abstractmethod
-    def bake(self):
-        pass
+class Builder(ABC):
     
+    @property
     @abstractmethod
-    def setSabor(self):
+    def product(self) -> None:
         pass
 
     @abstractmethod
-    def setTipo(self):
+    def putVela(self) -> None:
         pass
 
+    @abstractmethod
+    def putBoneco(self) -> None:
+        pass
+
+    @abstractmethod
+    def setTipo(self, tipo) -> None:
+        pass
+
+# Builder Concretos
+# -------------------------------------------------------
+
+class ChocolateBuilder(Builder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Chocolate()
+    
+    @property
+    def product(self) -> Chocolate:
+        product = self._product
+        self.reset()
+        return product
+    
+    def putVela(self) -> None:
+        self._product.add('Vela')
+
+    def putBoneco(self) -> None:
+        self._product.add('Boneco')
+
+    def setTipo(self, tipo) -> None:
+        self._product.setType(tipo)
+
+class MandiocaBuilder(Builder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Mandioca()
+    
+    @property
+    def product(self) -> Mandioca:
+        product = self._product
+        self.reset()
+        return product
+    
+    def putVela(self) -> None:
+        self._product.add('Vela')
+
+    def putBoneco(self) -> None:
+        self._product.add('Boneco')
+
+    def setTipo(self, tipo) -> None:
+        self._product.setType(tipo)
+
+class CenouraBuilder(Builder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._product = Cenoura()
+    
+    @property
+    def product(self) -> Cenoura:
+        product = self._product
+        self.reset()
+        return product
+    
+    def putVela(self) -> None:
+        self._product.add('Vela')
+
+    def putBoneco(self) -> None:
+        self._product.add('Boneco')
+
+    def setTipo(self, tipo) -> None:
+        self._product.setType(tipo)
+
+# Classes concretas
+# -------------------------------------------------------
+class Chocolate():
+    def __init__(self) -> None:
+        self.parts = []
+        self.type = []
+    
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+    
+    def setType(self, tipo):
+        self.type = tipo
+    
+    def __str__(self) -> str:
+        return f"Bolo de chocolate do tipo {self.type} com {', '.join(self.parts)}"
+
+class Mandioca():
+    def __init__(self) -> None:
+        self.parts = []
+        self.type = []
+    
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+    
+    def setType(self, tipo):
+        self.type = tipo
+    
+    def __str__(self) -> str:
+        return f"Bolo de Mandioca do tipo {self.type} com {', '.join(self.parts)}"
+
+class Cenoura():
+    def __init__(self) -> None:
+        self.parts = []
+        self.type = []
+    
+    def add(self, part: Any) -> None:
+        self.parts.append(part)
+    
+    def setType(self, tipo):
+        self.type = tipo
+    
+    def __str__(self) -> str:
+        return f"Bolo de Cenoura do tipo {self.type} com {', '.join(self.parts)}"
 
 
-class BoloBuilder(builder):
-    def __init__(self, sabor, tipo):
-        self.sabor = sabor
-        self.tipo = tipo
+# Criar diretor
+# -------------------------------------------------------
+class Director:
     
-    def setSabor(self, sabor):
-        self.sabor = sabor
+    def __init__(self) -> None:
+        self._builder = None
 
-    def setTipo(self, tipo):
-        self.tipo = tipo
+    @property
+    def builder(self) -> Builder:
+        return self._builder
+
+    @builder.setter
+    def builder(self, builder:Builder) -> None:
+        self._builder = builder
     
-    def bake(self):
-        return Bolo(self.sabor, self.tipo)
+    def bakeAniversario(self) -> None:
+        self._builder.setTipo("Aniversário")
+        self._builder.putVela()
     
+    def bakeCasamento(self) -> None:
+        self._builder.setTipo("Casamento")
+        self._builder.putBoneco()
+
+    def bakeInformal(self) -> None:
+        self._builder.setTipo("Informal")
+
+# A main vai funcionar como Client
+# -------------------------------------------------------
+if __name__ == "__main__":
+
+    director = Director()
+
+## ========== Chocolate ===================================
+    choco = ChocolateBuilder()
+    print("Bolos de Chocolate")
+    print("=============================")
+    director.builder = choco
+
+    director.bakeAniversario()
+    print(str(choco.product))
+
+    director.bakeCasamento()
+    print(choco.product)
+
+    director.bakeInformal()
+    print(choco.product)
+    print("\n")
+
+## ========== Mandioca ===================================
+    mandioca = MandiocaBuilder()
+    print("Bolos de Mandioca")
+    print("=============================")
+    director.builder = mandioca
+
+    director.bakeAniversario()
+    print(mandioca.product)
+
+    director.bakeCasamento()
+    print(mandioca.product)
+
+    director.bakeInformal()
+    print(Builder.product)
+    print("\n")
+
+## ========== Cenoura ===================================
+    cenoura = CenouraBuilder()
+    print("Bolos de Cenoura")
+    print("=============================")
+    director.builder = cenoura
+
+    director.bakeAniversario()
+    print(cenoura.product)
+
+    director.bakeCasamento()
+    print(cenoura.product)
+
+    director.bakeInformal()
+    print(cenoura.product)
+    print("\n")
